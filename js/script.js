9daa67b534e7603011452582831e5078482c4d46
steffalon <simon.boerrigter@gmail.com>
2017-10-20 12:37:04 +0200

    15 objects

---
+++ js/script.js
@@ -0,0 +1,78 @@
+let auto = {
+  aantalLighten: 4,
+  aantalDeuren: 3,
+  fabriekant: 'Porsche';
+}
+
+let macbook = {
+  toetsenbordLayout: 'QWERTY',
+  gpu: 'AMD R9 M390X',
+  merk: 'Apple';
+}
+
+let rugzak = {
+  kanOpen: true,
+  laptopVak: true,
+  merk: 'Samsunite';
+}
+
+let jas {
+  rits: true,
+  kleur: 'blauw';
+}
+
+let iphone6s {
+  kleur: 'zwart',
+  simkaart: 'micro';
+}
+
+let huisSluitels {
+  OpenVoorDeur: true,
+  kleur: 'Bronze';
+}
+
+let papierBlad {
+  formaat: 'A4',
+  vorm: 'rechthoek';
+}
+
+let stekkerContact {
+  looptStroomDoor: true,
+  kleur: "wit";
+}
+
+let portemonnee {
+  matriaal: "leer",
+  kleur: "zwart";
+}
+
+let deur {
+  matriaal: "hout",
+  heeftSlot: false;
+}
+
+let pinpas {
+  isGeldig: true,
+  bank: "ABN";
+}
+
+let geld {
+  eenheid: 'euro',
+  waarde: 5;
+}
+
+let oplader {
+  wattage: 85,
+  kleur: 'wit';
+}
+
+let schoenen {
+  kleur: "wit",
+  maat: 42;
+}
+
+let bril {
+  glazenvorm: 'rechthoek',
+  kleur: 'rood/zwart',
+  aantalGlazen: 2;
+}
